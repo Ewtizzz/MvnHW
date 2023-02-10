fdf6e3cb9a25d9b8c4db31974a8ebbacb7c96721
Ewtizzz <egor.zholobov@gmail.com>
2023-02-10 14:22:47 +0300

    сделано ДЗ

--- src/main/java/pro/sky/mvnhw/controller/FirstController.java
+++ src/main/java/pro/sky/mvnhw/controller/FirstController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstController {
 
+
     @GetMapping
     public String helloWorld(){
         return "приложение запущено";
     }
+
     @GetMapping("/info")
     public String Info(@RequestParam String name, String prName, Integer data, String desc){
         return "имя: " + name +
-        "название проекта: " + prName  +
-        "дата создания: " + data +
-        "описание: " + desc;
+                "название проекта: " + prName  +
+                "дата создания: " + data +
+                "описание: " + desc;
     }
 }
