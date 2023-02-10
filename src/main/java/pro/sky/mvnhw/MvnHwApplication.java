ad318fedadeb28172cb405f0a53e8ca097334998
Ewtizzz <egor.zholobov@gmail.com>
2023-02-10 13:38:28 +0300

    initial commit

---
+++ src/main/java/pro/sky/mvnhw/MvnHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.mvnhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/mvnhw/MvnHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mvnhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
